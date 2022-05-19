f0711e38835d889fb45b144186f70087e9b41027
Katerina Komarkova <58290791+kkomarkova@users.noreply.github.com>
2022-05-19 20:14:27 +0200

    Trophy

---
+++ Roll-Call-System-Neo4j/Controllers/TrophyController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Neo4jClient;
+using Roll_Call_System_Neo4j.Models;
+
+namespace Roll_Call_System_Neo4j.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TrophyController : ControllerBase
+    {
+        private readonly IGraphClient _client;
+        public TrophyController(IGraphClient client)
+        {
+            _client = client;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var users = await _client.Cypher.Match("(n: Trophy)")
+                            .Return(n => n.As<Trophy>()).ResultsAsync;
+            return Ok(users);
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateTrophy([FromBody] Trophy trophy)
+        {
+            await _client.Cypher.Create("(n:Trophy{automatic: $automatic,name: $name})")
+                                .WithParam("automatic", trophy.automatic)
+                                .WithParam("name", trophy.name)
+                                .ExecuteWithoutResultsAsync();
+            return Ok();
+        }
+        [HttpDelete("{name}")]
+        public async Task<IActionResult> DeleteTrophy(string name)
+        {
+            await _client.Cypher.Match("(t:Trophy)")
+                                 .Where((Trophy t) => t.name == name)
+                                 .Delete("t")
+                                 .ExecuteWithoutResultsAsync();
+            return Ok();
+
+        }
+        [HttpPut("{name}")]
+        public async Task<IActionResult> UpdateTrophy(string name, [FromBody] Trophy trophy)
+        {
+            await _client.Cypher.Match("(t:Trophy)")
+                                .Where((Trophy t) => t.name == name)
+                                .Set("t = $trophy")
+                                .WithParam("trophy", trophy)
+                                .WithParam("name", trophy.name)
+                                .ExecuteWithoutResultsAsync();
+            return Ok();
+        }
+    }
+}
---
+++ Roll-Call-System-Neo4j/Models/Trophy.cs
@@ -0,0 +1,8 @@
+﻿namespace Roll_Call_System_Neo4j.Models
+{
+    public class Trophy
+    {
+        public bool automatic { get; set; }
+        public string name { get; set; }
+    }
+}
