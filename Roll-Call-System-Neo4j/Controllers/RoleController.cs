7201a0149925bbc6bb538dfc1f1522a2bc003a12
Katerina Komarkova <58290791+kkomarkova@users.noreply.github.com>
2022-05-19 20:13:56 +0200

    Role

---
+++ Roll-Call-System-Neo4j/Controllers/RoleController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Neo4jClient;
+using Roll_Call_System_Neo4j.Models;
+
+namespace Roll_Call_System_Neo4j.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoleController : ControllerBase
+    {
+        private readonly IGraphClient _client;
+        public RoleController(IGraphClient client)
+        {
+            _client = client;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var trophies = await _client.Cypher.Match("(n: Role)")
+                            .Return(n => n.As<Role>()).ResultsAsync;
+            return Ok(trophies);
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateRole([FromBody] Role role)
+        {
+            await _client.Cypher.Create("(n:Role{name: $name})")
+                                .WithParam("name", role.name)
+                                .ExecuteWithoutResultsAsync();
+            return Ok();
+        }
+        [HttpPut("{name}")]
+        public async Task<IActionResult> UpdateRole(string name, [FromBody] Role role)
+        {
+            await _client.Cypher.Match("(r:Role)")
+                                .Where((Role r) => r.name == name)
+                                .Set("r = $role")
+                                .WithParam("role", role)
+                                .WithParam("name", role.name)
+                                .ExecuteWithoutResultsAsync();
+            return Ok();
+        }
+        [HttpDelete("{name}")]
+        public async Task<IActionResult> DeleteRole(string name)
+        {
+            await _client.Cypher.Match("(r:Role)")
+                                 .Where((Role r) => r.name == name)
+                                 .Delete("r")
+                                 .ExecuteWithoutResultsAsync();
+            return Ok();
+
+        }
+    }
+}
+
---
+++ Roll-Call-System-Neo4j/Models/Role.cs
@@ -0,0 +1,8 @@
+﻿namespace Roll_Call_System_Neo4j.Models
+{
+    public class Role
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+}
