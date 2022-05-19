a0a1e430f26df569e49f4542bb4803e8817f378c
Katerina Komarkova <58290791+kkomarkova@users.noreply.github.com>
2022-05-19 20:14:43 +0200

    Subject

---
+++ Roll-Call-System-Neo4j/Controllers/SubjectController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Neo4jClient;
+using Roll_Call_System_Neo4j.Models;
+
+namespace Roll_Call_System_Neo4j.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SubjectController : ControllerBase
+    {
+        private readonly IGraphClient _client;
+        public SubjectController(IGraphClient client)
+        {
+            _client = client;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var subjects = await _client.Cypher.Match("(n: Subject)")
+                            .Return(n => n.As<Subject>()).ResultsAsync;
+            return Ok(subjects);
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateSubject([FromBody] Subject subject)
+        {
+            await _client.Cypher.Create("(n:Subject{name: $name})")
+                                .WithParam("name", subject.name)
+                                .ExecuteWithoutResultsAsync();
+            return Ok();
+        }
+        [HttpPut("{name}")]
+        public async Task<IActionResult> UpdateSubject(string name, [FromBody] Subject subject)
+        {
+            await _client.Cypher.Match("(s:Subject)")
+                                .Where((Subject s) => s.name == name)
+                                .Set("s = $subject")
+                                .WithParam("subject", subject)
+                                .WithParam("name", subject.name)
+                                .ExecuteWithoutResultsAsync();
+            return Ok();
+        }
+        [HttpDelete("{name}")]
+        public async Task<IActionResult> DeleteSubject(string name)
+        {
+            await _client.Cypher.Match("(s:Subject)")
+                                 .Where((Subject s) => s.name == name)
+                                 .Delete("s")
+                                 .ExecuteWithoutResultsAsync();
+            return Ok();
+
+        }
+
+        //[HttpGet]
+        //public async Task<IActionResult> GetSubjectswithlessons()
+        //{
+        //    var subjects = await _client.Cypher.Match("(Lesson)-[r: IS_PART_OF_SUBJECT]->(n:Subject))")
+        //     .ReturnDistinct(n => new
+        //     {
+        //         Subject = n.As<Subject>(),
+        //         Lesson = n.As<Lesson>()}).ResultsAsync;
+        //    return Ok(subjects);
+
+        //}
+
+    }
+    }
+
---
+++ Roll-Call-System-Neo4j/Controllers/UserController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+//using Neo4j.Driver;
+using Neo4jClient;
+using Roll_Call_System_Neo4j.Models;
+
+namespace Roll_Call_System_Neo4j.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IGraphClient _client;
+        public UserController(IGraphClient client)
+        {
+            _client = client;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var users = await _client.Cypher.Match("(n: User)")
+                            .Return(n => n.As<User>()).ResultsAsync;
+            return Ok(users);
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateUser([FromBody] User user)
+        {
+            await _client.Cypher.Create("(n:User{email: $email, firstName: $firstName, lastName: $lastName, password: $password})")
+                                .WithParam("email", user.email)
+                                .WithParam("firstName", user.firstName)
+                                .WithParam("lastName", user.lastName)
+                                .WithParam("password", user.password)
+                                .ExecuteWithoutResultsAsync();
+            return Ok();
+        }
+        [HttpDelete("{email}")]
+        public async Task<IActionResult> DeleteUser(string email)
+        {
+            await _client.Cypher.Match("(u:User)")
+                                 .Where((User u) => u.email == email)
+                                 .Delete("u")
+                                 .ExecuteWithoutResultsAsync();
+            return Ok();
+
+        }
+        [HttpPut("{email}")]
+        public async Task<IActionResult> UpdateUser(string email, [FromBody] User user)
+        {
+            await _client.Cypher.Match("(u:User)")
+                                .Where((User u) => u.email == email)
+                                .Set("u = $user")
+                                .WithParam("user", user)
+                                .WithParam("email", user.email)
+                                .WithParam("firstName", user.firstName)
+                                .WithParam("lastName", user.lastName)
+                                .WithParam("password", user.password)
+                                .ExecuteWithoutResultsAsync();
+            return Ok();
+        }
+    }
+}
