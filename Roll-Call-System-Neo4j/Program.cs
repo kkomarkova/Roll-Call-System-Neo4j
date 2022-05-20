0a032917c0be748b69d3056856930e27db970abc
jakubmraz <jakub.mraz111@gmail.com>
2022-05-21 00:14:07 +0200

    Authentication (no authorisation yet)

--- Roll-Call-System-Neo4j/Program.cs
+++ Roll-Call-System-Neo4j/Program.cs
@@ -1,23 +1,64 @@
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Identity.Web;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 //using Neo4j.Driver;
 using Neo4jClient;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
+//builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+//    .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
 
 builder.Services.AddControllers();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "Jwt Authorization",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement{
+    {
+        new OpenApiSecurityScheme
+        {
+            Reference = new OpenApiReference
+            {
+                Type = ReferenceType.SecurityScheme,
+                Id = "Bearer"
+            }
+        },
+        new string[]{}
+
+        }
+    });
+
+});
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:key"]))
+    };
+});
 
 ////Neo4j connect to DB server
-var client = new BoltGraphClient(new Uri("neo4j+s://f7345ac5.databases.neo4j.io"),"neo4j", "EUg50eak7AVsWt-n_UGHeRXrXb2AKIKfq2HfdVPEFOQ");
+var client = new BoltGraphClient(new Uri("neo4j+s://f7345ac5.databases.neo4j.io"), "neo4j", "EUg50eak7AVsWt-n_UGHeRXrXb2AKIKfq2HfdVPEFOQ");
 client.ConnectAsync();
 builder.Services.AddSingleton<IGraphClient>(client);
 
